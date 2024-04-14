771347e4c736d5f6152d9e2d1728dc0bc452e6fc
Anastasiia Bulatkina <anstbulatkina@gmail.com>
2024-04-14 08:19:39 +0200

    The function of the encoding of the string

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,54 @@
 pub fn encode(n: u64) -> String {
-    unimplemented!("Say {n} in English.");
+    let ones = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    ];
+    let tens = [
+        "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
+
+    if n < 20 {
+        return ones[n as usize].to_string();
+    } else if n < 100 {
+        let mut result = tens[(n / 10 - 2) as usize].to_string();
+        if n % 10 != 0 {
+            result.push_str("-");
+            result.push_str(ones[(n % 10) as usize]);
+        }
+        return result;
+    } else if n < 1000 {
+        let mut result = ones[(n / 100) as usize].to_string();
+        result.push_str(" hundred");
+        if n % 100 != 0 {
+            result.push_str(" and ");
+            result.push_str(&encode(n % 100));
+        }
+        return result;
+    } else if n < 1000000 {
+        let mut result = encode(n / 1000);
+        result.push_str(" thousand");
+        if n % 1000 != 0 {
+            result.push_str(" ");
+            result.push_str(&encode(n % 1000));
+        }
+        return result;
+    } else if n < 1000000000 {
+        let mut result = encode(n / 1000000);
+        result.push_str(" million");
+        if n % 1000000 != 0 {
+            result.push_str(" ");
+            result.push_str(&encode(n % 1000000));
+        }
+        return result;
+    } else if n < 1000000000000 {
+        let mut result = encode(n / 1000000000);
+        result.push_str(" billion");
+        if n % 1000000000 != 0 {
+            result.push_str(" ");
+            result.push_str(&encode(n % 1000000000));
+        }
+        return result;
+    } else {
+        panic!("Number too large");
+    }
 }
